b0b66e69683f5e99ae94e5e0fa23f523d1fa74c3
Daniel <danielwinther@hotmail.dk>
2016-03-27 00:36:17 +0100

    fix

--- js/background.js
+++ js/background.js
@@ -1,37 +1,42 @@
+
+console.log("fsdfs");
+//showStreamers(localStorage["username"]);
+setInterval(function () {
     showStreamers(localStorage["username"]);
-    setInterval(function () {
-            showStreamers(localStorage["username"]);
-            chrome.browserAction.setBadgeText ( { text: onlineCounter.toString()});
-    }, 60000 * 15);
+    //chrome.browserAction.setBadgeText ( { text: onlineCounter.toString()});
+}, 60000 * 151);
 
 
 function showStreamers(username) {
-        onlineCounter = 0;
-        $.ajax({
-        url: "https://api.twitch.tv/kraken/users/" + username + "/follows/channels",
-        dataType: 'json',
-          success: function(data) {
-            $.each(data.follows, function (key, value) {
-                $.ajax({
-                    url: 'https://api.twitch.tv/kraken/streams/' + value.channel.name,
-                    dataType: 'json',
-                    success: function(data) {
-                        if (data.stream != null) {
-                            onlineCounter++;
-                            if (onlineCounter > 999) {
-                                chrome.browserAction.setBadgeText ( { text: "999+"});
-                            }
-                            else {
-                                chrome.browserAction.setBadgeText ( { text: onlineCounter.toString()});
-                            }
+    onlineCounter = 0;
+    $.ajax({
+    url: "https://api.twitch.tv/kraken/users/" + username + "/follows/channels",
+    dataType: 'json',
+      success: function(data) {
+        $.each(data.follows, function (key, value) {
+            $.ajax({
+                url: 'https://api.twitch.tv/kraken/streams/' + value.channel.name,
+                dataType: 'json',
+                success: function(data) {
+                    if (data.stream != null) {
+                        onlineCounter++;
+                        if (onlineCounter > 999) {
+                            chrome.browserAction.setBadgeText ( { text: "999+"});
                         }
-                      }
-                    });
+                        else {
+                            chrome.browserAction.setBadgeText ( { text: onlineCounter.toString()});
+                        }
+                    }
+                    else {
+                        chrome.browserAction.setBadgeText ( { text: onlineCounter.toString()});
+                    }
+                  }
                 });
-            },
-            error: function (error) {
-                localStorage.removeItem("username");
-                location.reload();
-            }
-        });
-    }
+            });
+        },
+        error: function (error) {
+            localStorage.removeItem("username");
+            location.reload();
+        }
+    });
+}
--- js/twitch.js
+++ js/twitch.js
@@ -33,7 +33,6 @@
                         if (data.stream != null) {
                             $("#online").append("<p><a class='text-success' target='_blank' href='" + value.channel.url + "'>" + value.channel.display_name + "</a></p>").hide().fadeIn(fadeInDuration);
                             if (data.stream != null) {
-
                             onlineCounter++;
                             if (onlineCounter > 999) {
                                 chrome.browserAction.setBadgeText ( { text: "999+"});
@@ -45,7 +44,6 @@
                         }
                         else {
                             chrome.browserAction.setBadgeText ( { text: onlineCounter.toString()});
-                        	console.log(onlineCounter.toString());
                             $("#offline").append("<p><a class='text-danger' target='_blank' href='" + value.channel.url + "'>" + value.channel.display_name + "</a></p>").hide().fadeIn(fadeInDuration);
                         }
                       }
