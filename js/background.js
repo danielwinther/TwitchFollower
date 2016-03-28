6332a1ff04459f00460efe4f7be624ee0a2555b3
Daniel <danielwinther@hotmail.dk>
2016-03-28 02:14:00 +0200

    badgefix

--- js/background.js
+++ js/background.js
@@ -1,28 +1,28 @@
 var app = angular.module('app', ['ngStorage']);
-app.controller('ctrl', function($scope, $localStorage, $interval, $http){
+app.constant('URL', 'https://api.twitch.tv/kraken/');
+app.constant('PARAMETERS', '?direction=DESC&limit=1000sortby=display_name');
+app.controller('ctrl', function($scope, $localStorage, $interval, $http, URL, PARAMETERS){
     $scope.runBackground = function() {
+        chrome.browserAction.setBadgeText ( { text: '0'});
         getTwitch();
         $interval(function(){
             getTwitch();
-        }, 60000 * 15);
+        }, 60000 * 10);
     };
     function getTwitch() {
         var online = new Array();
+        chrome.browserAction.setBadgeText ( { text: '0'});
 
-        $http.get('https://api.twitch.tv/kraken/users/' + ($localStorage.username ? $localStorage.username: "twitch")  + '/follows/channels?direction=DESC&limit=1000sortby=display_name')
+        $http.get(URL+ 'users/' + ($localStorage.username ? $localStorage.username: "twitch")  + '/follows/channels' + PARAMETERS)
         .then(function(response) {
             $scope.data = response.data.follows
             angular.forEach($scope.data, function(value, key){
-                $http.get('https://api.twitch.tv/kraken/streams/' + value.channel.display_name)
+                $http.get(URL + 'streams/' + value.channel.name)
                 .then(function(response) {
                     if (response.data.stream != null) {
                         online.push(value);
                         chrome.browserAction.setBadgeText ( { text: online.length.toString()});
                     }
-                    if (online.length == 0) {
-                        chrome.browserAction.setBadgeText ( { text: '0'});
-                    }
-                    console.log(online);
                 });
             });
         });
--- js/twitch.js
+++ js/twitch.js
@@ -1,17 +1,23 @@
 var app = angular.module('app', ['ngStorage']);
-app.controller('ctrl', function($scope, $localStorage, $interval, $http){
+app.constant('URL', 'https://api.twitch.tv/kraken/');
+app.constant('PARAMETERS', '?direction=DESC&limit=1000sortby=display_name');
+app.controller('ctrl', function($scope, $localStorage, $interval, $http, URL, PARAMETERS){
     $scope.username = $localStorage.username;
 
     $scope.getTwitch = function() {
+        try {
+            chrome.browserAction.setBadgeText ( { text: '0'});
+        }
+        catch(err) {}
+
         $localStorage.username = $scope.username;
         var online = new Array();
         var offline = new Array();
-
-        $http.get('https://api.twitch.tv/kraken/users/' + ($scope.username ? $scope.username : "twitch")  + '/follows/channels?direction=DESC&limit=1000sortby=display_name')
+        $http.get(URL + 'users/' + ($scope.username ? $scope.username : "twitch") + '/follows/channels' + PARAMETERS)
         .then(function(response) {
             $scope.data = response.data.follows
             angular.forEach($scope.data, function(value, key){
-                $http.get('https://api.twitch.tv/kraken/streams/' + value.channel.display_name)
+                $http.get(URL + 'streams/' + value.channel.name)
                 .then(function(response) {
                     if (response.data.stream != null) {
                         online.push(value);
